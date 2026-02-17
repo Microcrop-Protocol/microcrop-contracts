ece8bfc4850bd3b785255ae71448fa2837c40ff4
Antismart <timbwamoses83@gmail.com>
2026-02-17 12:09:56 +0300

    chore: add script to grant backend role

---
+++ microcrop/script/grant-backend-role.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cast send 0xDb6A11f23b8e357C0505359da4B3448d8EE5291C "grantRole(bytes32,address)" 0x25cf2b509f2a7f322675b2a5322b182f44ad2c03ac941a0af17c9b178f5d5d5f 0xC5867D3b114f10356bAAb7b77E04783cfA947c44 --rpc-url https://sepolia.base.org --account deployer
